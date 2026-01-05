f9df5ed92e876697bcc1b1a0c0ed898d442dd903
Devanshu <devanshup.pandey281@gmail.com>
2026-01-05 23:52:07 +0530

    restructure the project folder

--- apps/client/src/apis/socketInstance.jsx
+++
@@ -1,8 +0,0 @@
-import { io } from "socket.io-client";
-
-const server = import.meta.env.VITE_BASE_URL;
-
-export const socketInstance = io(`${server}`, {
-  transports: ["websocket"],
-  withCredentials: true,
-});
--- apps/server/src/sockets/queue.js
+++
@@ -1,8 +0,0 @@
-import redisConnection from "../utils/redisConnection.js";
-import { Queue } from "bullmq";
-
-export const bidQueue = new Queue("queue", {
-  connection: redisConnection,
-  removeOnComplete: true,
-  removeOnFail: true,
-});
